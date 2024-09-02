7480b335f34fc09e5da7d53c809a1911aefb78f4
OChernik <148587321+OChernik@users.noreply.github.com>
2024-09-02 06:40:28 +0300

    Update GarageFunctions.ino

--- GarageFunctions.ino
+++ GarageFunctions.ino
@@ -14,7 +14,7 @@ void initWiFi() {
 // и оставшееся время до импульса нагрева датчика
 void showScreen() {
   // counterDown это время, оставшееся до включения нагрева датчика SHT4x
-  float counterDown = (heat4xPeriod - (millis() - heat4xStart))/1000;
+  float counterDown = (heat4xPeriod - (millis() - heat4xStart)) / 1000;
   oled.clear();                      // очищаем дисплей
   oled.setScale(2);                  // масштаб текста (1..4)
   oled.setCursor(0, 0);              // курсор на начало 1 строки
@@ -24,12 +24,12 @@ void showScreen() {
   oled.print("T ");                  // вывод Т
   oled.print(temperatureGarage, 1);  // вывод значения Temperature
 
-  if (humidityGarage <= heat4xBorder) {       // если значение влажности меньше heat4xBorder
+  if (humidityGarage <= heat4xBorder) {         // если значение влажности меньше heat4xBorder
     oled.print((heat3xFlag) ? " On" : " Off");  // вывод "On" если датчик греется
-  } else {                                    // если значение влажности больше heat4xBorder
-    oled.print(" ");                    // вывод " " если датчик будет греться
-    oled.print(counterDown, 0);         // вывод значения времени до начала нагрева counterDown
-  }  // end IF
+  } else {                                      // если значение влажности больше heat4xBorder
+    oled.print(" ");                            // вывод " " если датчик будет греться
+    oled.print(counterDown, 0);                 // вывод значения времени до начала нагрева counterDown
+  }                                             // end IF
 
   oled.setCursor(0, 4);     // курсор на начало 3 строки
   oled.print("P ");         // вывод P
@@ -55,20 +55,22 @@ float checkValue(float newValue, float oldValue, int minValue, int maxValue, int
 void sendToOpenMon() {
   String buf;                                                             // Буфер для отправки
   buf.reserve(150);                                                       // Буфер для отправки
-  buf += F("http://open-monitoring.online/get?cid=3627&key=******=");  // формируем заголовок
+  buf += F("http://open-monitoring.online/get?cid=3627&key=*******=");  // формируем заголовок
   buf += temperatureOut;                                                  // вывод температуры улицы
   buf += F("&p2=");
-  buf += humidityOut;                                                     // вывод влажности улицы
+  buf += humidityOut;  // вывод влажности улицы
   buf += F("&p3=");
-  buf += temperatureGarage;                                               // вывод температуры гаража
+  buf += temperatureGarage;  // вывод температуры гаража
   buf += F("&p4=");
-  buf += humidityGarage;                                                  // вывод влажности в гараже
+  buf += humidityGarage;  // вывод влажности в гараже
   buf += F("&p5=");
-  buf += pressure;                                                        // вывод давления
+  buf += pressure;  // вывод давления
   buf += F("&p6=");
-  buf += temperatureBox;                                                  // вывод емпературы в корпусе
+  buf += temperatureBox;  // вывод температуры в корпусе
   buf += F("&p7=");
-  buf += rssi;                                                            // вывод силы сигнала Wi-Fi, dBm
+  buf += rssi;              // вывод силы сигнала Wi-Fi, dBm
+  buf += F("&p8=");
+  buf += carStatus;         // вывод статуса машины в гараже, есть или нет
   http.begin(buf.c_str());  // отправляем сформированную строку
   http.GET();               // Send HTTP GET request
   http.end();               // Free resources
@@ -82,26 +84,26 @@ void sendToNarodMon() {
   buf += F("#ESP32");
   buf += WiFi.macAddress();
   buf += F("\n");
-  buf.replace(":", "");  
+  buf.replace(":", "");
   Serial.println(buf);
   buf += F("#Temp1#");
   buf += temperatureOut;
-  buf += F("#Улица\n");  //NarodMon: вывод температуры на улице
+  buf += F("#Температура улица SHT41\n");  //NarodMon: вывод температуры на улице
   buf += F("#RH1#");
   buf += humidityOut;
-  buf += F("#Улица\n");  //NarodMon: вывод влажности на улице
+  buf += F("#Влажность улица SHT41\n");  //NarodMon: вывод влажности на улице
   buf += F("#Press#");
   buf += pressure;
-  buf += F("#Улица\n");  //NarodMon: вывод атмосферного давления
+  buf += F("#Атм. давление BMP280\n");  //NarodMon: вывод атмосферного давления
   buf += F("#Temp2#");
   buf += temperatureGarage;
-  buf += F("#Гараж\n");  //NarodMon: вывод температуры в гараже
+  buf += F("#Температура гараж\n");  //NarodMon: вывод температуры в гараже
   buf += F("#RH2#");
   buf += humidityGarage;
-  buf += F("#Гараж\n");  //NarodMon: вывод влажности в гараже  
+  buf += F("#Влажность гараж\n");  //NarodMon: вывод влажности в гараже
   buf += F("#DBM#");
   buf += rssi;
-  buf += F("#Гараж\n");                //NarodMon: вывод силы сигнала Wi-Fi, dBm
+  buf += F("#RSSI гараж\n");            //NarodMon: вывод силы сигнала Wi-Fi, dBm
   buf += F("##\n");                     //NarodMon: закрываем пакет
   client.connect("narodmon.ru", 8283);  //NarodMon: Подключаемся
   client.print(buf.c_str());            // И отправляем данные в сеть
@@ -110,31 +112,44 @@ void sendToNarodMon() {
 
 // функция закрывает ворота
 void closeGate() {
-  digitalWrite(relayGate, HIGH);    // для замыкания выставляем HIGH    
+  digitalWrite(relayGate, HIGH);  // для замыкания выставляем HIGH
   delay(500);
-  digitalWrite(relayGate, LOW);     // для размыкания выставляем LOW       
-  idleTimeTmr = millis();           // сброс таймера покоя
-  idleTime = 0;                     // сброс времени покоя
-} // end closeGate 
+  digitalWrite(relayGate, LOW);  // для размыкания выставляем LOW
+  idleTimeTmr = millis();        // сброс таймера покоя
+  idleTime = 0;                  // сброс времени покоя
+}  // end closeGate
 
-// функция опрашивает все датчики и устанавливает флаги
-void sensorsRead() {    
-    sht4x.measureHighPrecision(tempTemperature, tempHumidity);  // SensirionI2cSht4x.h
-    if ((millis() - heat4xStart) < 30000) {                     // сразу после нагрева выводим данные как есть
-      temperatureOut = tempTemperature;
-      humidityOut = hum4xCorrection + tempHumidity;
-    } else {  // через 30 секунд начинается фильтрация
-      temperatureOut = checkValue(tempTemperature, temperatureOut, -35, 40, 2);
-      humidityOut = hum4xCorrection + checkValue(tempHumidity, humidityOut, 20, 95, 2);
-    }
-    sht3x.measureSingleShot(REPEATABILITY_HIGH, false, tempTemperature, tempHumidity);  // SensirionI2cSht3x.h
-    temperatureGarage = checkValue(tempTemperature, temperatureGarage, -5, 35, 2);
-    humidityGarage = hum3xCorrection + checkValue(tempHumidity, humidityGarage, 20, 95, 2);
-    temperatureBox = checkValue(bme.readTemperature(), temperatureBox, 5, 45, 1);        // GyverBME280.h
-    pressure = checkValue((pressureToMmHg(bme.readPressure())), pressure, 720, 770, 1);  // GyverBME280.h
-    rssi = WiFi.RSSI();
-    bool newGateState = digitalRead(gercon);              // считали состояние ворот
-    if (newGateState != gateState) gateStateChanged = 1;  // если ворота открылись или закрылись, подняли флаг
-    else gateStateChanged = 0;                            // если нет, опустили флаг
-    gateState = newGateState;  // установили флаг состояния ворот    
+// функция считывает все датчики и выставляет флаги и таймер выезда машины carLeaveTmr
+void sensorsRead() {
+  sht4x.measureHighPrecision(tempTemperature, tempHumidity);  // SensirionI2cSht4x.h
+  if ((millis() - heat4xStart) < 30000) {                     // сразу после нагрева выводим данные как есть
+    temperatureOut = tempTemperature;
+    humidityOut = hum4xCorrection + tempHumidity;
+  } else {  // через 30 секунд начинается фильтрация
+    temperatureOut = checkValue(tempTemperature, temperatureOut, -35, 40, 2);
+    humidityOut = hum4xCorrection + tempHumidity;
+    // humidityOut = hum4xCorrection + checkValue(tempHumidity, humidityOut, 20, 95, 2);
+  }
+  sht3x.measureSingleShot(REPEATABILITY_HIGH, false, tempTemperature, tempHumidity);  // SensirionI2cSht3x.h
+  temperatureGarage = checkValue(tempTemperature, temperatureGarage, -5, 35, 2);
+  humidityGarage = hum3xCorrection + checkValue(tempHumidity, humidityGarage, 20, 95, 2);
+  temperatureBox = checkValue(bme.readTemperature(), temperatureBox, 5, 45, 1);        // GyverBME280.h
+  pressure = checkValue((pressureToMmHg(bme.readPressure())), pressure, 720, 770, 1);  // GyverBME280.h
+  rssi = WiFi.RSSI();
+
+  distance = sonar.ping_cm();                                 // определили расстояние до машины
+  bool newCarStatus;
+  (distance > 100)? (newCarStatus = 0) : (newCarStatus = 1);  // определили статус машины - в гараже или нет
+  if (newCarStatus < carStatus) carLeaveTmr = millis(); // если машина выехала, установили таймер  выезда машины из гаража
+  carStatus = newCarStatus;                             // установили статус машины
+
+  bool newGateState = digitalRead(gercon);              // считали состояние ворот
+  if (newGateState != gateState) gateStateChanged = 1;  // если ворота открылись или закрылись, подняли флаг
+  else gateStateChanged = 0;                            // если нет, опустили флаг
+  gateState = newGateState;                             // установили флаг состояния ворот 
+  
+  if (digitalRead(pir1)) pir1State = 1; else pir1State = 0;  // если появился сигнал на входе pir1
+  if (digitalRead(pir2)) pir2State = 1; else pir2State = 0;  // если появился сигнал на входе pir2
+  // если все датчики в состоянии покоя
+  if (!pir1State && !pir2State) idleState = 1; else idleState = 0;
 }
